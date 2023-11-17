2a640191514725357e77adfeb8e16b71734cee48
Jack-Eddy <144264179+Jack-Eddy@users.noreply.github.com>
2023-11-17 18:15:33 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,51 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Jack Eddy
+Created on: Nov 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
+# variables
+pixel_x = 0
+pixel_y = 0
 
-display.scroll("Hello, World!")
+display.show(Image.HAPPY)
+
+# button A pressed
+while True:
+    if button_a.is_pressed():
+        display.clear()
+        pixel_x = 0
+        pixel_y = 0
+        # move pixel on y axis down (outer loop)
+        while pixel_y <= 3:
+            # move pixel on x axis to the right (inner loop)
+            while pixel_x <= 3:
+                sleep(500)
+                display.clear()
+                display.set_pixel(pixel_x, pixel_y, 9)
+                pixel_x = pixel_x + 1
+            sleep(500)
+            display.clear()
+            display.set_pixel(pixel_x, pixel_y, 9)
+            pixel_y = pixel_y + 1
+        # move pixel on y axis up (outer loop)
+        while pixel_y >= 0:
+            # move pixel on x axis to the left (inner loop)
+            while pixel_x >= 1:
+                sleep(500)
+                display.clear()
+                display.set_pixel(pixel_x, pixel_y, 9)
+                pixel_x = pixel_x - 1
+            sleep(500)
+            display.clear()
+            display.set_pixel(pixel_x, pixel_y, 9)
+            pixel_y = pixel_y - 1
+
+        # reset screen and variables
+        sleep(500)
+        display.clear()
+        pixel_x = 0
+        pixel_y = 0
+        display.show(Image.HAPPY)
